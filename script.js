53597153b04f9ee33995b854d6a83c6f3039d7f7
Muhammad Alvi <149120789+MuhammadAlvi03@users.noreply.github.com>
2023-11-04 21:14:23 -0500

    add comments for plan

---
+++ script.js
@@ -0,0 +1,16 @@
+// rock, paper, scissors 
+// rock beats paper, loses to scissors
+// scissors beats paper, loses to rock
+// paper beats rock, loses to scissors
+
+// start with making a function 'getComputerChoice' that will return rock, paper, or scissors
+
+// then write a function that plays a single round of rock paper scissors; 
+// the function should take two parameters 'playerSelection' and 'computerSelection' and make them
+// return a string that declares the winner (for example "You lose! Paper beats rock.")
+// make function's 'playerSelection' parameter case insensitive so typing in 'ROCK' is the same as 'rock'
+// return the results of the function call so we can use this value
+
+
+// could start with RNG to return computer choice
+// then store the computer choice, and compare it to player choice
